6c526a745668e37c32037939774bc0116b63cfd8
Daniel Wehner <daniel@tag1consulting.com>
2014-11-28 14:23:33 +0100

    Ensure that the service container works in real life. Add an integration test for \ServiceContainer

---
+++ lib/Drupal/service_container/Tests/ServiceContainerIntegrationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\service_container\Tests\ServiceContainerIntegrationTest.
+ */
+
+namespace Drupal\service_container\Tests;
+
+class ServiceContainerIntegrationTest extends \DrupalWebTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'ServiceContainer',
+      'description' => 'Tests the \ServiceContainer class',
+      'group' => 'service_container',
+    );
+  }
+
+  protected function setUp() {
+    return parent::setUp(array('service_container'));
+  }
+
+  /**
+   * Tests some basic
+   */
+  public function testInit() {
+    \ServiceContainer::init();
+
+    $this->assertTrue(\Drupal::hasService('service_container'));
+    $this->assertTrue(\Drupal::hasService('module_handler'));
+    $this->assertTrue(\Drupal::hasService('module_installer'));
+  }
+
+}
+
