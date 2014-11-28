8084bdb77fea94685f317e39c24b38d76ea100ef
Daniel Wehner <daniel@tag1consulting.com>
2014-11-28 01:07:25 +0100

    Add a string translation implementation

---
+++ src/StringTranslation/StringTranslation.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\d8\StringTranslation\StringTranslation.
+ */
+
+namespace Drupal\d8\StringTranslation;
+
+use Drupal\Core\StringTranslation\TranslationInterface;
+
+/**
+ * Provides a t() based string translation.
+ */
+class StringTranslation implements TranslationInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function translate($string, array $args = array(), array $options = array()) {
+    return t($string, $args, $options);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formatPlural($count, $singular, $plural, array $args = array(), array $options = array()) {
+    return format_plural($count, $singular, $plural, $args, $options);
+  }
+
+}
