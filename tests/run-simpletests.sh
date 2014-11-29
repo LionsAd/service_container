a40fabd578c6e7c5c4811fa125294119d14a4edc
Fabian Franz <github@fabian-franz.de>
2014-11-29 04:58:53 +0100

    Make URI configurable.

--- tests/run-simpletests.sh
+++ tests/run-simpletests.sh
@@ -7,9 +7,6 @@ set -e
 # Store the module directory.
 export MODULE_DIR=$(cd $(dirname $0); cd ..; pwd)
 
-# Export the PATH to include the global composer namespace.
-export PATH=~/.composer/vendor/bin:$PATH
-
 # Goto current directory.
 DIR=$(dirname $0)
 cd $DIR
@@ -17,7 +14,7 @@ cd $DIR
 # Do we already have a local Drupal version?
 export FOUND=""
 { drush status | grep -q 'Drupal version'; } && FOUND="yes"
-FOUND=""
+URI=""
 
 if [ -z "$FOUND" ]
 then
@@ -31,6 +28,7 @@ then
 
         # Enable it to download dependencies.
 	drush --yes en service_container
+        URI="--uri=http://127.0.0.1:8080"
 fi
 
-drush test-run "service_container" --uri=http://127.0.0.1:8080
+drush test-run "service_container" $URI
