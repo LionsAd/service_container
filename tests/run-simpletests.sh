ee5aaf093d584b5c0b12a3301b430ab9d9ca3d56
Fabian Franz <github@fabian-franz.de>
2014-11-29 04:49:55 +0100

    Rework based on https://github.com/sonnym/travis-ci-drupal-module-example.

--- tests/run-simpletests.sh
+++ tests/run-simpletests.sh
@@ -17,26 +17,20 @@ cd $DIR
 # Do we already have a local Drupal version?
 export FOUND=""
 { drush status | grep -q 'Drupal version'; } && FOUND="yes"
+FOUND=""
 
 if [ -z "$FOUND" ]
 then
-	echo "No Drupal installation found. Installing via git."
+	echo "No Drupal installation found. Assuming /tmp/drupal_service_container via travis."
 
-        # Install in /tmp directory to prevent endless recursion.
-        cd /tmp/
-
-	# Install Drupal 7.x
-	git clone --branch 7.x --depth 1 http://git.drupal.org/project/drupal.git
-	cd drupal
+        # Find drupal installation in /tmp/.
+        cd /tmp/drupal_service_container/drupal/
 
 	# Point service_container into the drupal installation.
 	ln -sf "$MODULE_DIR" sites/all/modules/service_container
 
-	# Install it and run the tests.
-	drush --yes site-install minimal --db-url="sqlite://tmp/drupal_database"
-
-	drush --yes en simpletest
+        # Enable it to download dependencies.
 	drush --yes en service_container
 fi
 
-drush test-run "service_container"
+drush test-run "service_container" --uri=http://127.0.0.1:8080
