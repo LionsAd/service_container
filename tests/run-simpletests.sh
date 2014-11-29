bb42c0dfc430bf50eb26498fa8ea601603a467c8
Fabian Franz <github@fabian-franz.de>
2014-11-29 05:08:18 +0100

    Cleanup, remove php 5.3 for now, ensure directory is saved.

--- tests/run-simpletests.sh
+++ tests/run-simpletests.sh
@@ -4,9 +4,6 @@
 
 set -e
 
-# Store the module directory.
-export MODULE_DIR=$(cd $(dirname $0); cd ..; pwd)
-
 # Goto current directory.
 DIR=$(dirname $0)
 cd $DIR
@@ -14,21 +11,13 @@ cd $DIR
 # Do we already have a local Drupal version?
 export FOUND=""
 { drush status | grep -q 'Drupal version'; } && FOUND="yes"
-URI=""
 
 if [ -z "$FOUND" ]
 then
-	echo "No Drupal installation found. Assuming /tmp/drupal_service_container via travis."
+	echo "No Drupal installation found. Assuming /tmp/drupal_service_container via travis-ci."
 
         # Find drupal installation in /tmp/.
         cd /tmp/drupal_service_container/drupal/
-
-	# Point service_container into the drupal installation.
-	ln -sf "$MODULE_DIR" sites/all/modules/service_container
-
-        # Enable it to download dependencies.
-	drush --yes en service_container
-        URI="--uri=http://127.0.0.1:8080"
 fi
 
-drush test-run "service_container" $URI
+drush test-run "service_container" "$@"
