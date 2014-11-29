53d31c015f4197d5fc3561f40092befd2ea12e9c
Fabian Franz <github@fabian-franz.de>
2014-11-29 04:09:45 +0100

    Store correct directory; make it work with local installation.

--- tests/run-simpletests.sh
+++ tests/run-simpletests.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 # @file
-# Simple script to install drupal run the tests.
+# Simple script to run the tests, optionally installs minimal Drupal.
 
 set -e
 
+# Store the module directory.
+export MODULE_DIR=$(cd $(dirname $0); cd ..; pwd)
+
+# Goto current directory.
 DIR=$(dirname $0)
 cd $DIR
-# Install Drupal 7.x
-git clone --branch 7.x --depth 1 http://git.drupal.org/project/drupal.git
-cd drupal
 
-# Install the module dependencies.
-git clone --branch 7.x-1.x --depth 1 http://git.drupal.org/project/ctools.git sites/all/modules/ctools
-git clone --branch 7.x-1.x --depth 1 http://git.drupal.org/project/registry_autoload.git sites/all/modules/registry_autoload
+# Do we already have a local Drupal version?
+export FOUND=""
+{ drush status | grep -q 'Drupal version'; } && FOUND="yes"
 
-# Point service_container into the drupal installation.
-cp -r $DIR sites/all/modules/service_container
+if [ -z "$FOUND" ]
+then
+	echo "No Drupal installation found. Installing via git."
 
-PATH=~/.composer/vendor/bin:$PATH
+	# Install Drupal 7.x
+	git clone --branch 7.x --depth 1 http://git.drupal.org/project/drupal.git
+	cd drupal
 
-# Install it and run the tests.
-drush --yes site-install minimal --db-url="sqlite://tmp/drupal_database"
+	# Point service_container into the drupal installation.
+	ln -sf "$MODULE_DIR" sites/all/modules/service_container
 
-drush --yes en simpletest
-drush --yes en service_container
+	export PATH=~/.composer/vendor/bin:$PATH
+
+	# Install it and run the tests.
+	drush --yes site-install minimal --db-url="sqlite://tmp/drupal_database"
+
+	drush --yes en simpletest
+	drush --yes en service_container
+fi
 
 drush test-run "service_container" --debug
