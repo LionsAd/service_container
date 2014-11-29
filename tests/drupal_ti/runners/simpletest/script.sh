7ba96668139c04356ddd4d0e670d2dc2b49ebbdd
Fabian Franz <github@fabian-franz.de>
2014-11-29 15:30:21 +0100

    Fix args for simpletest.

--- tests/drupal_ti/runners/simpletest/script.sh
+++ tests/drupal_ti/runners/simpletest/script.sh
@@ -4,7 +4,9 @@
 
 set -e $DRUPAL_TI_DEBUG
 
+ARGS=( $DRUPAL_TI_SIMPLETEST_ARGS )
+
 cd "$TRAVIS_BUILD_DIR/../drupal_travis/drupal/"
-{ php scripts/run-tests.sh "$@" || echo "1 fails"; } | tee /tmp/simpletest-result.txt
+{ php scripts/run-tests.sh "${ARGS[@]}" || echo "1 fails"; } | tee /tmp/simpletest-result.txt
 
 egrep -i -v -q "([0-9]+ fails)|(PHP Fatal error)|([0-9]+ exceptions)" /tmp/simpletest-result.txt
