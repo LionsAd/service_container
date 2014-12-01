8b5e4c5d1300782a5d384f695225621ab1d3a688
Daniel Wehner <daniel@tag1consulting.com>
2014-12-01 13:12:06 +0100

    Implement specific factory overrides to not require the symfony container interface

--- src/ServiceContainer/ServiceProvider/ServiceContainerServiceProvider.php
+++ src/ServiceContainer/ServiceProvider/ServiceContainerServiceProvider.php
@@ -57,7 +57,7 @@ class ServiceContainerServiceProvider implements ServiceProviderInterface {
     $parameters['factory.keyvalue'] = array();
     $parameters['factory.keyvalue.expirable'] = array();
     $services['keyvalue'] = array(
-      'class' => 'Drupal\Core\KeyValueStore\KeyValueFactory',
+      'class' => 'Drupal\service_container\KeyValueStore\KeyValueFactory',
       'arguments' => array('@service_container', '%factory.keyvalue%')
     );
     $services['keyvalue.database'] = array(
@@ -65,7 +65,7 @@ class ServiceContainerServiceProvider implements ServiceProviderInterface {
       'arguments' => array('@serialization.phpserialize', '@database')
     );
     $services['keyvalue.expirable'] = array(
-      'class' => 'Drupal\Core\KeyValueStore\KeyValueExpirableFactory',
+      'class' => '\Drupal\service_container\KeyValueStore\KeyValueExpirableFactory',
       'arguments' => array('@service_container', '%factory.keyvalue.expirable%')
     );
     $services['keyvalue.expirable.database'] = array(
