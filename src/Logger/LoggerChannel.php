775043d55046e067ace22915757bafc673cbd4a7
Daniel Wehner <daniel@tag1consulting.com>
2014-12-02 19:35:00 +0100

    replace some of the custom code in LoggerChannel with the core code

--- src/Logger/LoggerChannel.php
+++ src/Logger/LoggerChannel.php
@@ -7,37 +7,14 @@
 
 namespace Drupal\service_container\Logger;
 
-use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Defines a logger channel that most implementations will use.
  */
-class LoggerChannel implements LoggerChannelInterface {
-
-  /**
-   * The name of the channel of this logger instance.
-   *
-   * @var string
-   */
-  protected $channel;
-
-  /**
-   * An array of arrays of \Psr\Log\LoggerInterface keyed by priority.
-   *
-   * @var \Psr\Log\LoggerInterface[][]
-   */
-  protected $loggers = array();
-
-  /**
-   * Constructs a LoggerChannel object
-   *
-   * @param string $channel
-   *   The channel name for this instance.
-   */
-  public function __construct($channel) {
-    $this->channel = $channel;
-  }
+class LoggerChannel extends \Drupal\Core\Logger\LoggerChannel implements LoggerChannelInterface {
 
   /**
    * {@inheritdoc}
@@ -54,87 +31,13 @@ class LoggerChannel implements LoggerChannelInterface {
   /**
    * {@inheritdoc}
    */
-  public function setLoggers(array $loggers) {
-    $this->loggers = $loggers;
+  public function setRequestStack(RequestStack $requestStack = NULL) {
   }
 
   /**
    * {@inheritdoc}
    */
-  public function addLogger(LoggerInterface $logger, $priority = 0) {
-    $this->loggers[$priority][] = $logger;
-  }
-
-  /**
-   * Sorts loggers according to priority.
-   *
-   * @return \Psr\Log\LoggerInterface[]
-   *   An array of sorted loggers by priority.
-   */
-  protected function sortLoggers() {
-    $sorted = array();
-    krsort($this->loggers);
-
-    foreach ($this->loggers as $loggers) {
-      $sorted = array_merge($sorted, $loggers);
-    }
-    return $sorted;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function emergency($message, array $context = array()) {
-    return $this->log(LogLevel::EMERGENCY, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function alert($message, array $context = array()) {
-    return $this->log(LogLevel::ALERT, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function critical($message, array $context = array()) {
-    return $this->log(LogLevel::CRITICAL, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function error($message, array $context = array()) {
-    return $this->log(LogLevel::ERROR, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function warning($message, array $context = array()) {
-    return $this->log(LogLevel::WARNING, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function notice($message, array $context = array()) {
-    return $this->log(LogLevel::NOTICE, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function info($message, array $context = array()) {
-    return $this->log(LogLevel::INFO, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function debug($message, array $context = array()) {
-    return $this->log(LogLevel::DEBUG, $message, $context);
+  public function setCurrentUser(AccountInterface $current_user = NULL) {
   }
 
 }
