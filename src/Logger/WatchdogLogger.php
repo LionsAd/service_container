11ce897486dad24b3b55b35d98a180f6b59bd429
Daniel Wehner <daniel@tag1consulting.com>
2014-12-03 12:19:16 +0100

    don't extend from the core class but at least use a base class to reduce the boilerplate

--- src/Logger/WatchdogLogger.php
+++ src/Logger/WatchdogLogger.php
@@ -15,63 +15,7 @@ use Psr\Log\LogLevel;
  *
  * @codeCoverageIgnore
  */
-class WatchdogLogger implements LoggerInterface {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function emergency($message, array $context = array()) {
-    return $this->log(LogLevel::EMERGENCY, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function alert($message, array $context = array()) {
-    return $this->log(LogLevel::ALERT, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function critical($message, array $context = array()) {
-    return $this->log(LogLevel::CRITICAL, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function error($message, array $context = array()) {
-    return $this->log(LogLevel::ERROR, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function warning($message, array $context = array()) {
-    return $this->log(LogLevel::WARNING, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function notice($message, array $context = array()) {
-    return $this->log(LogLevel::NOTICE, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function info($message, array $context = array()) {
-    return $this->log(LogLevel::INFO, $message, $context);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function debug($message, array $context = array()) {
-    return $this->log(LogLevel::DEBUG, $message, $context);
-  }
+class WatchdogLogger extends LoggerBase implements LoggerInterface {
 
   /**
    * {@inheritdoc}
