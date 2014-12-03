696fffd081cb380327587647c8ffda97022ae571
Daniel Wehner <daniel@tag1consulting.com>
2014-12-03 09:58:30 +0100

    refetch the account service from the container to ensure that we really test the right one

--- lib/Drupal/service_container/Tests/CurrentUserIntegrationTest.php
+++ lib/Drupal/service_container/Tests/CurrentUserIntegrationTest.php
@@ -25,16 +25,17 @@ class CurrentUserIntegrationTest extends ServiceContainerIntegrationTestBase {
   public function testCurrentUser() {
     /** @var \Drupal\service_container\Session\Account $account */
 
-    $created_account = $this->container->get('current_user');
-    $this->assertTrue($created_account instanceof Account);
+    $account_service = $this->container->get('current_user');
+    $this->assertTrue($account_service instanceof Account);
 
     $admin_user = $this->drupalCreateUser(array('access content'));
     $this->drupalLogin($admin_user);
     drupal_save_session(FALSE);
     $GLOBALS['user'] = $admin_user;
     $account = $this->container->get('current_user');
-    $this->assertEqual(spl_object_hash($created_account), spl_object_hash($account), 'Ensure that the object in the container stays the same.');
+    $this->assertEqual(spl_object_hash($account_service), spl_object_hash($account), 'Ensure that the object in the container stays the same.');
 
+    $this->assertEqual($admin_user->uid, $account->id());
     $this->assertEqual(array_keys($admin_user->roles), $account->getRoles());
     $this->assertEqual(1, count($account->getRoles(TRUE)));
     $this->assertEqual($admin_user->name, $account->getUsername());
