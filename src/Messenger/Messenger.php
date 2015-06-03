9b6a7f114a6d7c25fffe6b5543c7e0fc4265225f
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-03 14:56:11 +0200

    Remove non necessary files.

---
+++ src/Messenger/Messenger.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\service_container\Messenger.
+ */
+
+namespace Drupal\service_container\Messenger;
+
+/**
+ * TODO
+ *
+ * @codeCoverageIgnore
+ */
+class Messenger extends LegacyMessenger {}
