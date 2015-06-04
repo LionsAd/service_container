552e4a9744ee6ae2dab90fc76e98dac07b72f454
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-04 11:21:11 +0200

    First essay of a test.

---
+++ lib/Drupal/service_container/Tests/LegacyMessengerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\service_container\Tests\CurrentUserIntegrationTest.
+ */
+
+namespace Drupal\service_container\Tests;
+
+use Drupal\service_container\Messenger\LegacyMessenger;
+
+class LegacyMessengerTest extends ServiceContainerIntegrationTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'LegacyMessenger',
+      'description' => 'Tests the messenger integration',
+      'group' => 'service_container',
+    );
+  }
+
+  public function testLegacyMessenger() {
+    /** @var \Drupal\service_container\Messenger\LegacyMessenger $messenger_service */
+
+    $messenger_service = $this->container->get('messenger');
+    $this->assertTrue($messenger_service instanceof LegacyMessenger);
+
+    $random_message = $this->randomString();
+
+    drupal_set_message($random_message, $messenger_service::WARNING);
+
+    $messages = $messenger_service->getMessages();
+    $warning_messages = $messages[$messenger_service::WARNING];
+    $this->assertTrue(in_array($random_message, $warning_messages));
+  }
+
+}
