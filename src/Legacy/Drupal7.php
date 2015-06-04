e222502403758236cd992c644b9d80e6773000c2
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-04 15:16:10 +0200

    Add @codeCoverageIgnore

--- src/Legacy/Drupal7.php
+++ src/Legacy/Drupal7.php
@@ -7,6 +7,9 @@
 
 namespace Drupal\service_container\Legacy;
 
+/**
+ * @codeCoverageIgnore
+ */
 class Drupal7 {
   public function __call($method, $args) {
     return call_user_func_array($method, $args);
