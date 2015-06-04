146f7c98e45e0abb146ba0820abb2416f61ced42
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-04 10:15:28 +0200

    Add missing @inheritdoc

--- src/Messenger/LegacyMessenger.php
+++ src/Messenger/LegacyMessenger.php
@@ -13,23 +13,38 @@ namespace Drupal\service_container\Messenger;
  * @codeCoverageIgnore
  */
 class LegacyMessenger implements MessengerInterface {
+  /**
+   * {@inheritdoc}
+   */
   public function addMessage($message, $type = self::STATUS, $repeat = FALSE) {
     drupal_set_message($message, $type, $repeat);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getMessages() {
     return drupal_get_messages();
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getMessagesByType($type) {
     $messages = $this->getMessages();
     return isset($messages[$type]) ? $messages[$type] : $messages;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function deleteMessages() {
     throw new \BadMethodCallException('LegacyMessenger::deleteMessages is not implemented.');
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function deleteMessagesByType($type) {
     throw new \BadMethodCallException('LegacyMessenger::deleteMessagesByType is not implemented.');
   }
