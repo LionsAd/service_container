bf9eb378a53ec3dacba074ccf1a8e1f5ee044992
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-04 23:53:02 +0200

    Add missing methods and missing class to fix tests.

--- src/Extension/ModuleInstaller.php
+++ src/Extension/ModuleInstaller.php
@@ -8,6 +8,7 @@
 namespace Drupal\service_container\Extension;
 
 use Drupal\Core\Extension\ModuleInstallerInterface;
+use Drupal\Core\Extension\ModuleUninstallValidatorInterface;
 
 /**
  * Provides a module installer compatible with D7.
@@ -29,6 +30,20 @@ class ModuleInstaller implements ModuleInstallerInterface {
   public function uninstall(array $module_list, $uninstall_dependents = TRUE) {
     module_disable($module_list, $uninstall_dependents);
     drupal_uninstall_modules($module_list);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addUninstallValidator(ModuleUninstallValidatorInterface $uninstall_validator) {
+    throw new \BadMethodCallException(sprintf('%s is not implemented', __FUNCTION__));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateUninstall(array $module_list) {
+    throw new \BadMethodCallException(sprintf('%s is not implemented', __FUNCTION__));
   }
 
 }
