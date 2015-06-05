abfb11c5c3febcf75d96b3bd3cac7f76a43e3a04
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-05 11:56:08 +0200

    Tests update.

--- tests/src/Messenger/LegacyMessengerTest.php
+++ tests/src/Messenger/LegacyMessengerTest.php
@@ -19,7 +19,7 @@ class LegacyMessengerTest extends \PHPUnit_Framework_TestCase {
    *
    * @var \Drupal\service_container\Legacy\Drupal7
    */
-  protected $drupal7_service;
+  protected $drupal7;
 
   /**
    * The Messenger service.
@@ -34,41 +34,91 @@ class LegacyMessengerTest extends \PHPUnit_Framework_TestCase {
   protected function setUp() {
     parent::setUp();
 
-    $this->drupal7_service = new Drupal7();
-    $this->messenger_service = new LegacyMessenger($this->drupal7_service);
+    // We can't use the mock object here because LegacyMessenger needs a
+    // \Drupal\service_container\Legacy\Drupal7 argument.
+    // Do we need to create an interface for it instead ?
+    $this->drupal7 = new Drupal7();
+    $this->messenger_service = new LegacyMessenger($this->drupal7);
   }
 
   /**
    * @covers ::__construct()
    */
   public function test_construct() {
-    $this->assertInstanceOf('\Drupal\service_container\Messenger\LegacyMessenger', $this->messenger_service);
-    $this->assertInstanceOf('\Drupal\service_container\Legacy\Drupal7', $this->drupal7_service);
+    $this->assertInstanceOf('\Drupal\service_container\Messenger\MessengerInterface', $this->messenger_service);
+    $this->assertInstanceOf('\Drupal\service_container\Legacy\Drupal7', $this->drupal7);
   }
 
   /**
    * @covers ::addMessage()
    */
-  public function test_addMessage() {}
+  public function test_addMessage() {
+    $drupal7 = \Mockery::mock('Drupal\service_container\Legacy\Drupal7');
+    $message = 'Izumi';
+    $type = 'warning';
+
+    $drupal7
+      ->shouldReceive('drupal_set_message')
+      ->once()
+      ->with($message, $type, FALSE);
+
+    $this->messenger_service->addMessage($message, $type);
+  }
 
   /**
    * @covers ::getMessages()
    */
-  public function test_getMessages() {}
+  public function test_getMessages() {
+    $drupal7 = \Mockery::mock('Drupal\service_container\Legacy\Drupal7');
+
+    $drupal7
+      ->shouldReceive('drupal_get_messages')
+      ->once()
+      ->with(NULL, FALSE);
+
+    $this->messenger_service->getMessages();
+  }
 
   /**
    * @covers ::getMessagesByType()
    */
-  public function test_getMessagesByType() {}
+  public function test_getMessagesByType() {
+    $drupal7 = \Mockery::mock('Drupal\service_container\Legacy\Drupal7');
+
+    $drupal7
+      ->shouldReceive('drupal_get_messages')
+      ->once()
+      ->with('warning', FALSE);
+
+    $this->messenger_service->getMessages('warning');
+  }
 
   /**
    * @covers ::deleteMessages()
    */
-  public function test_deleteMessages() {}
+  public function test_deleteMessages() {
+    $drupal7 = \Mockery::mock('Drupal\service_container\Legacy\Drupal7');
+
+    $drupal7
+      ->shouldReceive('drupal_get_messages')
+      ->once()
+      ->with(NULL, TRUE);
+
+    $this->messenger_service->deleteMessages();
+  }
 
   /**
    * @covers ::deleteMessagesByType()
    */
-  public function test_deleteMessagesByType() {}
+  public function test_deleteMessagesByType() {
+    $drupal7 = \Mockery::mock('Drupal\service_container\Legacy\Drupal7');
+
+    $drupal7
+      ->shouldReceive('drupal_get_messages')
+      ->once()
+      ->with('warning', TRUE);
+
+    $this->messenger_service->deleteMessagesByType('warning');
+  }
 
 }
