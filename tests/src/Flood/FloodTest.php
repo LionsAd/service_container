9aa7aa3bcc7a1c3eb0e20c0a537c56e27a02d2c1
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-08 12:03:38 +0200

    Fix __construct again.

--- tests/src/Flood/FloodTest.php
+++ tests/src/Flood/FloodTest.php
@@ -7,7 +7,6 @@
 
 namespace Drupal\Tests\service_container\Flood;
 
-use Drupal\Core\Database\Database;
 use Drupal\service_container\Flood\DatabaseBackend;
 use Drupal\service_container\Legacy\Drupal7;
 
@@ -30,7 +29,7 @@ class FloodTest extends \PHPUnit_Framework_TestCase {
     parent::setUp();
 
     $drupal7 = new Drupal7();
-    $connection = Database::getConnection();
+    $connection = \Database::getConnection();
 
     $this->flood_service = new DatabaseBackend($connection, $drupal7);
   }
