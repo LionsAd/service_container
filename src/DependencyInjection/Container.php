4a4adcd954424420b3d96f92a52d1966ec21e62f
Fabian Franz <github@fabian-franz.de>
2015-06-10 04:15:24 -0700

    Fix code coverage.

--- src/DependencyInjection/Container.php
+++ src/DependencyInjection/Container.php
@@ -400,6 +400,8 @@ class Container implements ContainerInterface {
    *
    * @return string
    *   The camelized string.
+   *
+   * @codeCoverageIgnore
    */
   public static function camelize($name) {
     return strtr(ucwords(strtr($name, array('_' => ' ', '\\' => '_ '))), array(' ' => ''));
@@ -413,6 +415,8 @@ class Container implements ContainerInterface {
    *
    * @return string
    *   The underscored string.
+   *
+   * @codeCoverageIgnore
    */
   public static function underscore($name) {
     return strtolower(preg_replace(array('/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'), array('\\1_\\2', '\\1_\\2'), $name));
