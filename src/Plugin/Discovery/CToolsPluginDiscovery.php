411bfdaa23cc95febc5fb9ede75b2a5bcd5d81c3
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-16 14:13:13 +0200

    Fix typo.

--- src/Plugin/Discovery/CToolsPluginDiscovery.php
+++ src/Plugin/Discovery/CToolsPluginDiscovery.php
@@ -13,7 +13,7 @@ use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 /**
  * A discovery mechanism that uses ctools plugins for Drupal 7 compatibility.
  *
- * This class cannot be tested as it relies on the existance of procedural code.
+ * This class cannot be tested as it relies on the existence of procedural code.
  * @codeCoverageIgnore
  */
 class CToolsPluginDiscovery implements DiscoveryInterface {
