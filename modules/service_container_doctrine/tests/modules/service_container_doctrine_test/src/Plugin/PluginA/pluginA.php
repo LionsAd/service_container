1ad8fff16f47a81b2efe3fec012fcab0ce9cc6dc
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-06-18 17:15:24 +0200

    Add module file for service_container_doctrine.

---
+++ modules/service_container_doctrine/tests/modules/service_container_doctrine_test/src/Plugin/PluginA/pluginA.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\service_container_doctrine_test\Plugin\PluginA;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Class pluginA
+ *
+ * @Plugin(
+ *   id = "PluginA",
+ *   label = "Label PluginA"
+ * )
+ *
+ * @package Drupal\service_container_doctrine_test\Plugin\PluginA
+ */
+class pluginA {
+
+}
