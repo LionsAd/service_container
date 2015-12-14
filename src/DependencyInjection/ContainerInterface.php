59e38a812f5d0a80b6d4e6ec080c27e902795d0b
Fabian Franz <github@fabian-franz.de>
2015-12-14 02:55:45 -0800

    Remove no longer needed definitions.

--- src/DependencyInjection/ContainerInterface.php
+++ src/DependencyInjection/ContainerInterface.php
@@ -15,26 +15,4 @@ use Symfony\Component\DependencyInjection\IntrospectableContainerInterface;
  * @ingroup dic
  */
 interface ContainerInterface extends DiscoveryInterface, IntrospectableContainerInterface {
-
-  /**
-   * Returns a parameter from the container.
-   *
-   * @param string $name
-   *   The name of the parameter to retrieve.
-   *
-   * @return mixed
-   *   Returns the parameter with the given name.
-   */
-  public function getParameter($name);
-
-  /**
-   * Checks if a parameter exists in the container.
-   *
-   * @param string $name
-   *   The parameter name.
-   *
-   * @return bool
-   *   TRUE if the parameter exists, FALSE otherwise.
-   */
-  public function hasParameter($name);
 }
